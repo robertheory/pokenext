47ca27c797450121267d27be065435c9520b23a0
Roberto Costa <roberto.araujo339@gmail.com>
2022-11-06 20:32:04 -0300

    Searchbar navigate function

--- src/components/Layout/Searchbar.tsx
+++ src/components/Layout/Searchbar.tsx
@@ -15,6 +15,11 @@ const Searchbar = () => {
     return names.filter((p) => p.includes(search));
   }, [search]);
 
+  function handleSelectPokemon(name: string) {
+    setSearch('');
+    router.push(`/pokemon/${name}`);
+  }
+
   return (
     <>
       <Flex
@@ -28,6 +33,7 @@ const Searchbar = () => {
         align='center'
         position='relative'
         gap={2}
+        border='1px solid rgba(0,0,0,0.2)'
       >
         <SearchIcon color='#3f3f3f' />
         <Input
@@ -35,39 +41,43 @@ const Searchbar = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <Stack
-          position='absolute'
-          top='30px'
-          right='0px'
-          direction='column'
-          w='300px'
-          justify='flex-start'
-          align='stretch'
-          bg='white'
-          spacing={4}
-          borderRadius='8px'
-        >
-          {searchResults.map((resut) => (
-            <Text
-              w='100%'
-              key={resut}
-              color='#3f3f3f'
-              fontSize='1.2rem'
-              fontWeight='300'
-              textAlign='center'
-              borderRadius='8px'
-              cursor='pointer'
-              textTransform='capitalize'
-              p='2'
-              _hover={{
-                bg: '#b5b5b5',
-              }}
-              onClick={() => router.push(`/pokemon/${resut}`)}
-            >
-              {resut}
-            </Text>
-          ))}
-        </Stack>
+        {searchResults.length > 0 && (
+          <Stack
+            position='absolute'
+            top='30px'
+            right='0px'
+            direction='column'
+            w='300px'
+            justify='flex-start'
+            align='stretch'
+            bg='white'
+            spacing={4}
+            borderRadius='8px'
+            border='1px solid rgba(0,0,0,0.2)'
+            borderTop='none'
+          >
+            {searchResults.map((result) => (
+              <Text
+                w='100%'
+                key={result}
+                color='#3f3f3f'
+                fontSize='1.2rem'
+                fontWeight='300'
+                textAlign='center'
+                borderRadius='8px'
+                cursor='pointer'
+                textTransform='capitalize'
+                p='2'
+                _hover={{
+                  bg: '#b5b5b5',
+                }}
+                onClick={() => handleSelectPokemon(result)}
+              >
+                {result}
+              </Text>
+            ))}
+          </Stack>
+        )}
       </Flex>
     </>
   );
